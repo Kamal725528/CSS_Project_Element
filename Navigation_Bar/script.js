c2938d5235a02d81ccf35b23f3748212edb578dd
kamal725528 <kamalsingh725528@gmail.com>
2024-09-26 04:21:53 +0530

    grid

---
+++ Navigation_Bar/script.js
@@ -0,0 +1,10 @@
+// // script.js
+
+// document.addEventListener('DOMContentLoaded', function () {
+//     const menuButton = document.querySelector('.menu-button');
+//     const nav = document.querySelector('.nav');
+
+//     menuButton.addEventListener('click', function () {
+//         nav.classList.toggle('active');
+//     });
+// });
